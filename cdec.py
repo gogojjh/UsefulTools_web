f0d434065a50eac0d09ecc16db642280b32bc99e
pilipala195 <531744278@qq.com>
2015-12-24 10:46:32 +0800

    change CDEC's url

--- cdec.py
+++ cdec.py
@@ -22,15 +22,15 @@ def run():
         'Cookie': 'VPLang=zh_cn; VPTimeZone=Asia%2FChongqing; Hm_lvt_2e2626841280635cc9322ed4817ffce5=1448277535,1448277571,1448277622,1448461941; Hm_lvt_20c4cdf230856f4a4479a32ec8b13dd6=1448277529; VPSSO=4c5059ba9ed15b7c91228d1bd85d506dd7af041ca%3A4%3A%7Bi%3A0%3Bs%3A7%3A%223957696%22%3Bi%3A1%3Bs%3A11%3A%22pilipala195%22%3Bi%3A2%3Bi%3A518400%3Bi%3A3%3Ba%3A0%3A%7B%7D%7D; VPSessionID=2t1jggph5c47cpk2g4ttvn0kn6; Hm_lpvt_2e2626841280635cc9322ed4817ffce5=1448462040',
         'Connection': 'keep-alive'
     }
-    url = 'http://dota2.vpgame.com/api/dota2LeagueMatch-rank.html?pageSize=10&showType=page' +\
-          '&type=overview&season_id=554&_=1449545032706&DotaLeagueSeasonMemberStatistics_page=1'
+    url = 'http://dota2.vpgame.com/api/dota2LeagueMatch-rank.html?pageSize=10&showType=' +\
+          'page&type=overview&season_id=589&_=1450924655551&DotaLeagueSeasonMemberStatistics_page=1'
     ht = requests.get(url, headers=head).content
     jsDict = json.loads(ht)
     jsPage = jsDict['page']
     total_page = jsPage['totalPage']+1
     for i in range(1, total_page):
-        url = 'http://dota2.vpgame.com/api/dota2LeagueMatch-rank.html?pageSize=10&showType=page' +\
-              '&type=overview&season_id=554&_=1449545032706&DotaLeagueSeasonMemberStatistics_page='+str(i)
+        url = 'http://dota2.vpgame.com/api/dota2LeagueMatch-rank.html?pageSize=10&showType=' +\
+              'page&type=overview&season_id=589&_=1450924655551&DotaLeagueSeasonMemberStatistics_page='+str(i)
         ht = requests.get(url, headers=head).content
         jsDict = json.loads(ht)
         jsData = jsDict['data']
